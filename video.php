6d38debcaf2096335c4cdc9e84b949e3592edb95
ramankumar <raman.kumar@cimpress.com>
2020-03-13 14:59:53 +0530

    added

--- video.php
+++ video.php
@@ -83,7 +83,7 @@ if ($bid == 1 || $bid == "1") {
                                <video poster="<?=$imageBucket;?><?=$id;?>" width="100%" height="415" controls>
                                  <source src='<?=$movieBucket;?><?=$id;?>'   type="video/mp4" />
                                Your browser does not support the video tag.
-                               <track label="English" kind="subtitles" srclang="en" src="<?=$movieBucket;?><?=$id;?>.vtt" default>
+                               <track label="English" kind="subtitles" srclang="en" src="captions/vtt/jumanji.vtt" default>
                                </video>
                            </div>
                            <div class="single-video-title box mb-3">
