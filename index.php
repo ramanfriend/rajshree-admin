4d05ce1baba18bbf5965c0212a690dc9af3c8ca0
ramanfriend <ramanmarch90@gmail.com>
2020-04-18 12:06:08 +0530

    added

--- index.php
+++ index.php
@@ -40,7 +40,7 @@
          <button class="btn btn-link btn-sm text-secondary order-1 order-sm-0" id="sidebarToggle">
          <i class="fas fa-bars"></i>
          </button> &nbsp;&nbsp;
-         <a class="navbar-brand mr-1" href="index.html"><img class="img-fluid" alt="" src="img/logo.png"></a>
+         <a class="navbar-brand mr-1" href="/"><img class="img-fluid" alt="" src="img/logo.png"></a>
          <!-- Navbar Search -->
          <form class="d-none d-md-inline-block form-inline ml-auto mr-0 mr-md-5 my-2 my-md-0 osahan-navbar-search">
             <div class="input-group">
